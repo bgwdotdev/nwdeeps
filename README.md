f532ee596db6b98cfbbf1c5ccf47f07ec8d3b62f
bgw <bgw@bgw.dev>
2025-02-04 22:30:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# nwdeeps
+
+[![Package Version](https://img.shields.io/hexpm/v/nwdeeps)](https://hex.pm/packages/nwdeeps)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/nwdeeps/)
+
+```sh
+gleam add nwdeeps@1
+```
+```gleam
+import nwdeeps
+
+pub fn main() {
+  // TODO: An example of the project in use
+}
+```
+
+Further documentation can be found at <https://hexdocs.pm/nwdeeps>.
+
+## Development
+
+```sh
+gleam run   # Run the project
+gleam test  # Run the tests
+```
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              gleam
+              erlang_27
+              rebar3
+              websocat
+            ];
+          };
+        }
+      );
+}
